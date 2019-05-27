186c512e1abf065e2333937ec171b7fd8e36e0db
btl <bertrand.thill@francetv.fr>
2019-05-27 11:18:15 +0200

    Test without auth

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(
     name = "thumbor_mongodb_loader",
     packages = ["thumbor_mongodb_loader"],
-    version = "1.2.0",
+    version = "1.2.1",
     description = "MongoDB GridFS loader for Thumbor - Jeunesse Francetv Release",
     author = "Bertrand Thill",
     author_email = "bertrand.thill@francetv.fr",
