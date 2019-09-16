cd30fd95a3819058c9967c096147c5c3398bd8f9
btl <bertrand.thill@francetv.fr>
2019-09-16 11:02:53 +0200

    Modification du callback. Retourne un code d'erreur correct en cas d'abscence de l'image (Code 404 et non 500)

--- thumbor_mongodb_loader/loader.py
+++ thumbor_mongodb_loader/loader.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import gridfs
 import urllib
+from thumbor.loaders import LoaderResult
 
 def __conn__(self):
     the_database = self.config.MONGO_ORIGIN_SERVER_DB
@@ -20,15 +21,19 @@ def __conn__(self):
     #database
     db = client[self.config.MONGO_ORIGIN_SERVER_DB]
     return db
-    
+
 @return_future
 def load(self, path, callback):
     db = __conn__(self)
     words2 = path.split("/")
     storage = self.config.MONGO_ORIGIN_SERVER_COLLECTION
     images = gridfs.GridFS(db, collection=storage)
+    result = LoaderResult()
     if images.exists(ObjectId(words2[0])):
         contents = images.get(ObjectId(words2[0])).read()
-        callback(contents)
+        result.successful = True
+        result.buffer = contents
     else:
-        callback(None)
+        result.error = LoaderResult.ERROR_NOT_FOUND
+        result.successful = False
+    callback(result)
